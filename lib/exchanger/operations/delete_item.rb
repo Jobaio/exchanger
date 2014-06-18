cec55d3182b83bbb49bbb7471b026767e59526bd
Hunter Cassidy <hunter.cassidy@jobaio.com>
2014-06-18 15:16:39 -0400

    Add in initial support for meeting cancellations

--- lib/exchanger/operations/delete_item.rb
+++ lib/exchanger/operations/delete_item.rb
@@ -11,7 +11,7 @@ module Exchanger
   # http://msdn.microsoft.com/en-us/library/aa580484.aspx
   class DeleteItem < Operation
     class Request < Operation::Request
-      attr_accessor :item_ids
+      attr_accessor :item_ids, :send_meeting_cancellations
 
       # Reset request options to defaults.
       def reset
@@ -22,7 +22,7 @@ module Exchanger
         Nokogiri::XML::Builder.new do |xml|
           xml.send("soap:Envelope", "xmlns:soap" => NS["soap"], "xmlns:t" => NS["t"], "xmlns:xsi" => NS["xsi"], "xmlns:xsd" => NS["xsd"]) do
             xml.send("soap:Body") do
-              xml.DeleteItem("xmlns" => NS["m"], "DeleteType" => "HardDelete") do
+              xml.DeleteItem("xmlns" => NS["m"], "DeleteType" => "HardDelete", "SendMeetingCancellations" => send_meeting_cancellations) do
                 xml.ItemIds do
                   item_ids.each do |item_id|
                     xml["t"].ItemId("Id" => item_id)
